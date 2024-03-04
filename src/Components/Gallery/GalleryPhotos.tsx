1bfc5d2ea1a89a1d7488700133a51723c3356a5a
JPAlkamim <jpalkamim@gmail.com>
2024-03-04 20:52:39 -0300

    Arrumando importação

--- src/Components/Gallery/GalleryPhotos.tsx
+++ src/Components/Gallery/GalleryPhotos.tsx
@@ -22,21 +22,21 @@ export const GalleryPhotos = () => {
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3" />
                             </svg>
                         </div>
-                        <p  className='font-bold text-4xl my-4' style={{color: '#292822', fontFamily: 'Barlow'}}>
+                        <p  className='font-bold text-4xl my-4' style={{color: '#292822', fontFamily: 'barlowRegular'}}>
                         Copa Ricardo Gordo | 11/02/24 | Sarandi-PR</p>
                         <div className="flex">
                             <div className="mr-auto">
-                            <p className='text-xl mb-5' style={{color: '#292822', fontFamily: 'Barlow'}}>Califórnia FC x Monte Rei e Nego Stylo x Seg Amaral se enfrentaram pela Copa Ricardo Gordo de Futebol Suíço, na Arena Barba Rala, em Sarandi-PR
+                            <p className='text-xl mb-5' style={{color: '#292822', fontFamily: 'barlowRegular'}}>Califórnia FC x Monte Rei e Nego Stylo x Seg Amaral se enfrentaram pela Copa Ricardo Gordo de Futebol Suíço, na Arena Barba Rala, em Sarandi-PR
                             </p>
                             </div>
-                            <p className='text-xl font-bold mb-5' style={{color: '#AA591C', fontFamily: 'Barlow'}}>Contribua para este álbum</p>
+                            <p className='text-xl font-bold mb-5' style={{color: '#AA591C', fontFamily: 'barlowRegular'}}>Contribua para este álbum</p>
                         </div>
 
                         <PhotoAlbum layout='rows' photos={images} />
 
-                        <p  className='font-bold text-4xl my-4' style={{color: '#292822', fontFamily: 'Barlow'}}>
+                        <p  className='font-bold text-4xl my-4' style={{color: '#292822', fontFamily: 'barlowRegular'}}>
                         Contribua com este álbum!</p>
-                        <p className='text-xl mb-5' style={{color: '#292822', fontFamily: 'Barlow'}}>Faça o upload de suas fotos e vídeos sobre este jogo para enriquecer ainda mais o conteúdo.</p>
+                        <p className='text-xl mb-5' style={{color: '#292822', fontFamily: 'barlowRegular'}}>Faça o upload de suas fotos e vídeos sobre este jogo para enriquecer ainda mais o conteúdo.</p>
                     </div>
                 )
             }
@@ -48,34 +48,34 @@ export const GalleryPhotos = () => {
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3" />
                             </svg>
                         </div>
-                        <p  className='font-bold text-4xl my-4' style={{color: '#292822', fontFamily: 'Barlow'}}>
+                        <p  className='font-bold text-4xl my-4' style={{color: '#292822', fontFamily: 'barlowRegular'}}>
                         Copa Land View | 18/02/24 | Maringá-PR</p>
                         <div className="flex">
                             <div className="mr-auto">
-                            <p className='text-xl mb-5' style={{color: '#292822', fontFamily: 'Barlow'}}>O Bigbol Society, em Maringá-PR, recebeu o primeiro dia de competições da Copa Land View, organizada pela Amistosos Eventos
+                            <p className='text-xl mb-5' style={{color: '#292822', fontFamily: 'barlowRegular'}}>O Bigbol Society, em Maringá-PR, recebeu o primeiro dia de competições da Copa Land View, organizada pela Amistosos Eventos
                             </p>
                             </div>
-                            <p className='text-xl font-bold mb-5' style={{color: '#AA591C', fontFamily: 'Barlow'}}>Contribua para este álbum</p>
+                            <p className='text-xl font-bold mb-5' style={{color: '#AA591C', fontFamily: 'barlowRegular'}}>Contribua para este álbum</p>
                         </div>
 
                         <PhotoAlbum layout='rows' photos={images2} />
 
-                        <p  className='font-bold text-4xl my-4' style={{color: '#292822', fontFamily: 'Barlow'}}>
+                        <p  className='font-bold text-4xl my-4' style={{color: '#292822', fontFamily: 'barlowRegular'}}>
                         Contribua com este álbum!</p>
-                        <p className='text-xl mb-5' style={{color: '#292822', fontFamily: 'Barlow'}}>Faça o upload de suas fotos e vídeos sobre este jogo para enriquecer ainda mais o conteúdo.</p>
+                        <p className='text-xl mb-5' style={{color: '#292822', fontFamily: 'barlowRegular'}}>Faça o upload de suas fotos e vídeos sobre este jogo para enriquecer ainda mais o conteúdo.</p>
                     </div>
                 )
             }
             {
             photoDetail == '' && (
                 <div className='mt-64 mb-40 mx-24 xl:mx-44'>
-                    <p  className='font-bold text-4xl my-4' style={{color: '#292822', fontFamily: 'Barlow'}}>
+                    <p  className='font-bold text-4xl my-4' style={{color: '#292822', fontFamily: 'barlowBold'}}>
                         Galeria de Fotos</p>
                     <div className='grid grid-cols-2 gap-2 justify-items-center mt-20'>
                         <div className="cursor-pointer" onClick={() => setPhotoDetail('Gallery1')}>
                             <img src={capa1} alt="image1" className="w-80 h-56 m-auto transition duration-300 ease-in-out hover:shadow-lg dark:hover:shadow-black/30" />
                             <div className="grid justify-center">
-                                <p className='text-lg font-bold text-center' style={{color: '#292822', fontFamily: 'Barlow'}}>Copa Ricardo Gordo | 11/02/24 | Sarandi-PR</p>
+                                <p className='text-lg font-bold text-center' style={{color: '#292822', fontFamily: 'barlowRegular'}}>Copa Ricardo Gordo | 11/02/24 | Sarandi-PR</p>
                                 <p className='text-base text-center italic' style={{color: '#292822', fontFamily: 'barlowItalic'}}>Califórnia FC x Monte Rei e Nego Stylo x Seg Amaral se enfrentaram pela Copa Ricardo Gordo de Futebol Suíço, na Arena Barba Rala, em Sarandi-PR
                                 </p>
                             </div>
@@ -83,7 +83,7 @@ export const GalleryPhotos = () => {
                         <div className="cursor-pointer" onClick={() => setPhotoDetail('Gallery2')}>
                             <img src={capa2} alt="image1" className="w-80 h-56 m-auto transition duration-300 ease-in-out hover:shadow-lg dark:hover:shadow-black/30" />
                             <div className="grid justify-center">
-                                <p className='text-lg font-bold text-center' style={{color: '#292822', fontFamily: 'Barlow'}}>Copa Land View | 18/02/24 | Maringá-PR</p>
+                                <p className='text-lg font-bold text-center' style={{color: '#292822', fontFamily: 'barlowRegular'}}>Copa Land View | 18/02/24 | Maringá-PR</p>
                                 <p className='text-base text-center italic' style={{color: '#292822', fontFamily: 'barlowItalic'}}>O Bigbol Society, em Maringá-PR, recebeu o primeiro dia de competições da Copa Land View, organizada pela Amistosos Eventos</p>
                             </div>
                         </div>
--- src/Components/Gallery/MainGallery.tsx
+++ src/Components/Gallery/MainGallery.tsx
@@ -17,7 +17,7 @@ export const MainGallery = () => {
     return (
     <>
         <div className='mt-48 mb-24 mx-24 xl:mx-44'>
-            <p  className='font-bold text-4xl my-4' style={{color: '#292822', fontFamily: 'barlowRegular'}}>Galeria</p>
+            <p  className='font-bold text-4xl my-4' style={{color: '#292822', fontFamily: 'barlowBold'}}>Galeria</p>
             <div className="grid grid-cols-2 gap-52 justify-items-center">
                 <div className='relative' onClick={handlePhotoDetail}>
                     <img src={photo} alt="logo" className="cursor-pointer w-screen transition duration-300 ease-in-out hover:shadow-lg dark:hover:shadow-black/30" style={{height: '500px', width: '400px'}} />
